2735210732427f12e09d7d20e971466a31a3dc3b
Jonathan Price <pricetx@hotmail.com>
2013-12-19 18:30:53 +0000

    Update listplayers.lua
    Added header including name, license and version number.

--- listplayers/server/listplayers.lua
+++ listplayers/server/listplayers.lua
@@ -1,3 +1,7 @@
+-- Script to list number and names of players to console or chat
+-- Original Author, Jonathan Price
+-- License can be found at https://github.com/Pricetx/listplayers
+-- Version: 0.1
 class 'ListPlayers'
 
 -- If a player types "/list"
