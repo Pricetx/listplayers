70e2274e4f261ab846c387f0b90e7bda31dd64e0
Jonathan Price <pricetx@hotmail.com>
2013-12-19 18:24:41 +0000

    Update listplayers.lua
    -Added support for multiple names on one line in the server console.
    -Fixed colouring from black to a shade of blue
    -Fixed players to player(s) in the server console
    -Added comments for the List function

--- listplayers/server/listplayers.lua
+++ listplayers/server/listplayers.lua
@@ -4,28 +4,38 @@ class 'ListPlayers'
 OnPlayerChat = function(args)
     if args.text == "/list" then
         local stringlist = ""
-        Chat:Send(args.player, "There are " .. Server:GetPlayerCount() .. " player(s) connected:", Color(0,153,255,0))
-        for player in Server:GetPlayers() do
+        Chat:Send(args.player, "There are " .. Server:GetPlayerCount() .. " player(s) connected:", Color(0,153,255))
 
+        for player in Server:GetPlayers() do
             -- If the string is already at or over 80 characters, start a new line
             if string.len(stringlist) >= 80 then
-                Chat:Send(args.player, stringlist, Color(0,153,255,0))
+                Chat:Send(args.player, stringlist, Color(0,153,255))
                 stringlist = player:GetName() .. ", "
             else
                 stringlist = stringlist .. player:GetName() .. ", "
             end
         end
         -- string.sub removes the trailing comma
-        Chat:Send(args.player, string.sub(stringlist,0,-1), Color(0,153,255,0))
+        Chat:Send(args.player, string.sub(stringlist,0,-3), Color(0,153,255))
     end
 end
 
 -- If somebody in the server console types "list"
 List = function()
-    print("There are " .. Server:GetPlayerCount() .. " players connected:")
+    local stringlist = ""
+    print("There are " .. Server:GetPlayerCount() .. " player(s) connected:")
+
     for player in Server:GetPlayers() do
-        print(player:GetName())
+        -- If the string is already at or over 80 characters, start a new line
+        if string.len(stringlist) >= 80 then
+            print(stringlist)
+            stringlist = player:GetName() .. ", "
+        else
+            stringlist = stringlist .. player:GetName() .. ", "
+        end
     end
+    -- string.sub removes the trailing comma
+    print(string.sub(stringlist,0,-3))
 end
 
 Events:Subscribe("PlayerChat", OnPlayerChat)
