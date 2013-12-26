24a09a359e8d39d887c2569b9317826dcf415257
Eraknelo <rene.sackers@live.com>
2013-12-26 05:35:08 +0100

    Updated code
    Class was initialized (class 'ListPlayers') but never actually used.
    Class now has an init field (__init), this code is executed when the
    class is created.
    A class variable has been created (chatColor), that can now easily be
    changed to indicate the chat color.
    Also optimizes ferformance by only having to create a color once.
    
    Moved PlayerChat and list command event functions to the class.
    
    Added a class initializer, so the class actually gets created, and the
    initializer gets called.

--- server/listplayers.lua
+++ server/listplayers.lua
@@ -4,28 +4,40 @@
 -- Version: 0.1
 class 'ListPlayers'
 
--- If a player types "/list"
-OnPlayerChat = function(args)
+-- This code executes when the class is created
+function ListPlayers:__init()
+	-- Text color used in chat messages
+	self.chatColor = Color(0,153,255)
+
+	-- Subscribe to the player chat event, in the class's function "OnPlayerChat"
+	Events:Subscribe("PlayerChat", self, self.OnPlayerChat)
+	
+	-- Subscribe to the console command "list"
+	Console:Subscribe("list", self, self.List)
+end
+
+function ListPlayers:OnPlayerChat(args)
+	-- Player typed "/list"
     if args.text == "/list" then
         local stringlist = ""
-        Chat:Send(args.player, "There are " .. Server:GetPlayerCount() .. " player(s) connected:", Color(0,153,255))
+        Chat:Send(args.player, "There are " .. Server:GetPlayerCount() .. " player(s) connected:", self.chatColor)
 
         for player in Server:GetPlayers() do
             -- If the string is already at or over 80 characters, start a new line
             if string.len(stringlist) >= 80 then
-                Chat:Send(args.player, stringlist, Color(0,153,255))
+                Chat:Send(args.player, stringlist, self.chatColor)
                 stringlist = player:GetName() .. ", "
             else
                 stringlist = stringlist .. player:GetName() .. ", "
             end
         end
         -- string.sub removes the trailing comma
-        Chat:Send(args.player, string.sub(stringlist,0,-3), Color(0,153,255))
+        Chat:Send(args.player, string.sub(stringlist,0,-3), self.chatColor)
     end
 end
 
--- If somebody in the server console types "list"
-List = function()
+-- Executes when the command "list" is used
+function ListPlayers:List()
     local stringlist = ""
     print("There are " .. Server:GetPlayerCount() .. " player(s) connected:")
 
@@ -42,5 +54,5 @@ List = function()
     print(string.sub(stringlist,0,-3))
 end
 
-Events:Subscribe("PlayerChat", OnPlayerChat)
-Console:Subscribe("list", List)
+-- Initialize the class "ListPlayers". If we don't do this, the __init() function will never be called
+local listPlayers = ListPlayers()
